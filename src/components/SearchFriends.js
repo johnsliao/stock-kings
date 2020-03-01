f398944ff19056a4bf653153bbaa92b08d0f10c8
johnsliao <john@johnliao.org>
2020-03-01 11:11:37 -0500

    WIP

---
+++ src/components/SearchFriends.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SearchFriends extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Search Friends Page</h2>
+      </div>
+    );
+  }
+}
+
+export default SearchFriends;
