da868b4f85748931dd7b3e0bf5e8d4a91eef6679
xiaofanha <32694484+xiaofanha@users.noreply.github.com>
2020-03-04 05:57:58 -0500

    Update Register.js
    just simple register UI

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core'
+
 
 class Register extends Component {
   render() {
     return (
       <div>
-        <h2>Register</h2>
+        <h2>Register Page</h2>
+        <TextField id="outlined-size-normal" label="UserName" variant="outlined"/>
+        <div>
+        </div>
+        <TextField id="outlined-secondary" label="Email" variant="outlined"/>
+        <div>
+        </div>
+        <TextField id="outlined-secondary" label="Password" variant="outlined"/>
+        <div>
+        </div>
+        <TextField id="outlined-secondary" label="Confirm" variant="outlined"/>
+        <div>
+        </div>
+        <Button variant="contained" color="primary" > Finish </Button>
       </div>
     );
   }
 }
+
 
 export default Register;
