0cdf428d0d43018ccb04148257f5299979c535de
Zihao Li <horus@bu.edu>
2020-03-04 13:20:45 -0500

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 import { TextField } from '@material-ui/core'
 
 
