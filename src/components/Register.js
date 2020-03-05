716427e6e3162a41b6a0323414c14eab6b73edaf
xiaofanha <32694484+xiaofanha@users.noreply.github.com>
2020-03-04 19:50:17 -0500

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,26 +1,62 @@
 import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import { TextField } from '@material-ui/core'
+import { styled } from '@material-ui/core/styles';
 
+const MyButton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+});
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
 
 class Register extends Component {
   render() {
     return (
       <div>
         <h2>Register Page</h2>
-        <TextField id="outlined-size-normal" label="UserName" variant="outlined"/>
+        <TextField id="outlined-size-normal"
+                   label="UserName"
+                   variant="outlined"
+        />
         <div>
         </div>
-        <TextField id="outlined-secondary" label="Email" variant="outlined"/>
+        <TextField id="outlined-secondary"
+                   label="Email"
+                   variant="outlined"
+        />
         <div>
         </div>
-        <TextField id="outlined-secondary" label="Password" variant="outlined"/>
+        <TextField id="filled-password-input"
+                   label="Password"
+                   type="password"
+                   autoComplete="current-password"
+                   variant="filled"
+        />
         <div>
         </div>
-        <TextField id="outlined-secondary" label="Confirm" variant="outlined"/>
+        <TextField id="filled-password-input"
+                   label="Confirm"
+                   type="password"
+                   autoComplete="current-password"
+                   variant="filled"
+        />
         <div>
         </div>
-        <Button variant="contained" color="primary" > Finish </Button>
+        <MyButton>Next</MyButton>
       </div>
     );
   }
