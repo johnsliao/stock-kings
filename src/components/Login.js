bfc8378ede0ffd6b6d6a1572dacbbc67522c4c0b
Zihao Li <horus@bu.edu>
2020-03-05 15:01:05 -0500

    Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
+import {TextField} from "@material-ui/core";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -48,27 +49,34 @@ class Login extends Component {
     return (
       <div>
         <h2>Login Page</h2>
-          <input type="text" placeholder="username" name="username" value={this.state.username} onChange={this.onChange} />
-          <br/>
-          <input type="password" placeholder="password" name="password" value={this.state.password} onChange={this.onChange} />
-          <br/>
-          <div class="col-xs-8">
-            <input type="checkbox" name="remember" id="remember"/> Remember me
-          </div>
-            <Button onClick={this.handlecancel} variant="outlined">Cancel</Button>
+        <TextField id="outlined-size-normal"
+                   label="UserName"
+                   variant="outlined"
+                   style={{minWidth: "300px", maxWidth: "300px"}}
+                   helperText=" "
+        /><br/>
+        <TextField id="filled-password-input"
+                   label="Password"
+                   type="password"
+                   autoComplete="current-password"
+                   variant="filled"
+                   style={{minWidth: "300px", maxWidth: "300px"}}
+                   helperText=" "
 
-            <Button onClick={this.handlelogin} variant="outlined">Login</Button>
-            <Snackbar
-              open={this.state.open}
-              autoHideDuration={6000}
-              onClose={this.handleClose}
-            >
-              <Alert onClose={this.handleClose} severity="success">
-                Log in successfully!
-              </Alert>
-            </Snackbar>
-        </div>
-        
+        /><br/>
+        <input type="checkbox" name="remember" id="remember"/> Remember me<br/><br/>
+        <Button onClick={this.handlecancel} variant="outlined">Cancel</Button>
+        <Button onClick={this.handlelogin} variant="outlined">Login</Button>
+        <Snackbar
+          open={this.state.open}
+          autoHideDuration={1000}
+          onClose={this.handleClose}
+        >
+        <Alert onClose={this.handleClose} severity="success">
+          Log in successfully!
+        </Alert>
+        </Snackbar>
+      </div>
     );
   }
 }
